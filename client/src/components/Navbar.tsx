9cfc94bb52f0b9bbedbe94d3de4868f13fa14667
Tish254 <tiegomseeraoscar295@gmail.com>
2023-06-16 21:34:31 +0300

    style: Create Navbar Component; Fixed styling issues by adding a div to separate button and link content; branch; origin/development

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -40,22 +40,32 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="sticky top-0 h-10 left-0 right-0 py-[30px]  flex items-center justify-between z-20">
+    <nav className="sticky top-0 h-10 left-0 right-0 mt-[5px] py-[30px]  flex items-center justify-between z-20">
       <Link to="/" className="ml-[20px] lg:ml-[80px]">
         <Logo className="hidden lg:block" />
         <LogoIconSmall className="lg:hidden" />
       </Link>
-      <ul
-        className={`absolute lg:static top-[62px] left-0 right-0 ${
-          /** toggleNavigation ? "flex": "hidden" */ ""
-        }  flex flex-col lg:flex-row gap-[20px] z-20 bg-white left-0 px-[20px] py-[10px] h-[calc(100vh - 86px)] lg:px-0 h-screen lg:h-fit border-t-[1px] lg:border-0`}
-      >
-        {navItems.map(({ id, path, name }) => (
-          <Button key={id} type="navlink" active={active === id ? true : false} onClick={() => SetActiveNavLink(id)}>
-            <NavLink to={path}>{name}</NavLink>
-          </Button>
-        ))}
-      </ul>
+      <div className={`absolute lg:static top-[62px] left-0 right-0 ${
+            toggleNavigation ? "flex": "hidden"
+          }  flex-col lg:flex lg:flex-row gap-[20px] z-20 bg-white px-[20px] py-[10px] h-[calc(100vh-84px)] lg:px-0 lg:h-fit border-t-[1px] lg:border-0`}>
+        <ul
+          className={`flex flex-col lg:flex lg:flex-row gap-[20px] z-20 bg-white lg:px-0 lg:h-fit `}
+        >
+          {navItems.map(({ id, path, name }) => (
+            <Button key={id} type="navlink" active={active === id ? true : false} onClick={() => SetActiveNavLink(id)}>
+              <NavLink to={path}>{name}</NavLink>
+            </Button>
+          ))}
+        </ul>
+        <div className="lg:hidden mt-auto">
+        <Button>
+          <p className="flex items-center gap-2">
+            Get Started <HyphenWhite />{" "}
+            <span className="font-medium"> It's free</span>{" "}
+          </p>
+        </Button>
+      </div>
+      </div>
       <div className="hidden lg:block lg:mr-[80px]">
         <Button>
           <p className="flex items-center gap-2">
